4f856d565716405fd6adeede1920b5d7ea5f036e
stex0121 <160754478+stex0121@users.noreply.github.com>
2024-02-29 13:26:38 +0100

    Add files via upload

---
+++ calculator terminal.py
@@ -0,0 +1,53 @@
+    
+x=input(str("unesite x:"))
+y=input(str("unesite y:"))
+k=(int(x))
+z=(int(y))
+i=(k+z)
+print("zbir je : ", i)
+
+x=input(str("unesite x:"))
+y=input(str("unesite y:"))
+k=(int(x))
+z=(int(y))
+i=(k-z)
+print("razlika je : ", i)
+
+
+
+x=input(str("unesite x:"))
+y=input(str("unesite y:"))
+k=(int(x))
+z=(int(y))
+i=(k*z)
+print("proizvod je : ", i)
+
+x=input(str("unesite x:"))
+y=input(str("unesite y:"))
+k=(int(x))
+z=(int(y))
+i=(k/z)
+print("delilac je: ", i)
+
+x=input(str("unesite x:"))
+y=input(str("unesite y:"))
+k=(int(x))
+z=(int(y))
+i=(k**z)
+print("kvadrat je : ", i)
+
+x=input(str("unesite x:"))
+k=(int(x))
+import math
+i=math.sqrt(k)
+print("koren je : ", i)
+
+
+
+x=input(str("unesite x:"))
+y=input(str("unesite procenat:"))
+
+k=(int(x))
+i=(int(y))
+z=(k*(i/100))
+print("procenat je :", z)
