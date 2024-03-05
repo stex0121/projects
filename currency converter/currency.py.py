c58ec19b62244a5918c39e92b2d5182fe3984fdf
stex0121 <160754478+stex0121@users.noreply.github.com>
2024-03-05 17:27:24 +0100

    Add files via upload

--- currency converter/currency.py.py
+++ currency converter/currency.py.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root=Tk()
-root.geometry('320x300')
+root.geometry('320x500')
 root.title('currency converter')
 root.configure(bg='#808080')
 
@@ -9,13 +9,17 @@ def convert():
     cn1.delete(0,END)
     value=int(cn.get())
     value_1=value * 117.17
+    value_2=value * 107,84
     cn1.insert(0,str(value_1))
+    cn2.insert(0,str(value_2))
 
    
    
 label = Label(root,text="Currency Converter",relief="raised",borderwidth=2,font= ('arial',25),bg='#fff',padx=20,pady=10)
 label.pack(padx=10,pady=10)
 label1 =Label(root,text="EUR",font= ('arial',25),bg='#fff')
+label1.pack(padx=2,pady=2)
+label1 =Label(root,text="USD",font= ('arial',25),bg='#fff')
 label1.pack(padx=2,pady=2)
 cn = Entry(root,highlightthickness=1, width=30)
 cn.pack(padx=2,pady=2)
@@ -25,5 +29,7 @@ label2=Label(root,text="RSD",font= ('arial',25),bg='#fff')
 label2.pack(padx=2,pady=2)
 cn1=Entry(root,highlightthickness=1, width=30)
 cn1.pack(padx=2,pady=2)
+cn2=Entry(root,highlightthickness=1, width=30)
+cn2.pack(padx=2,pady=2)
 
 root.mainloop()
