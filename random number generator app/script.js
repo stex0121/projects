ce474523d55e7e02646c3294b8215441d7814f6a
stex0121 <160754478+stex0121@users.noreply.github.com>
2024-06-13 20:37:29 +0200

    Add files via upload

---
+++ random number generator app/script.js
@@ -0,0 +1,11 @@
+function generateRandomNumbers(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+document.getElementById("generateButton").addEventListener("click",function(){
+    var min=1;
+    var max=100;
+
+    var randomNumber=generateRandomNumbers(min,max);
+    document.getElementById("result").textContent=randomNumber;
+});
