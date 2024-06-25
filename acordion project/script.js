55b1037bdf585a2c4fb81d77314a76951b0439e8
stex0121 <160754478+stex0121@users.noreply.github.com>
2024-06-25 14:10:29 +0200

    Add files via upload

---
+++ acordion project/script.js
@@ -0,0 +1,6 @@
+const accordions=document.querySelectorAll(".accordion");
+accordions.forEach((accordion)=>{
+    accordion.addEventListener("click",()=>{
+        accordion.classList.toggle("active");
+    });
+});
