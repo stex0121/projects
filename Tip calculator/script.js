9d11dc4c5615f88f01cd46a9bb394c0e96b4fda9
stex0121 <160754478+stex0121@users.noreply.github.com>
2024-06-28 13:05:26 +0200

    Add files via upload

---
+++ Tip calculator/script.js
@@ -0,0 +1,14 @@
+function calculateTip(){
+    const billAmount=parseFloat(document.getElementById("bill-amount").value);
+    const tipPerecentage=parseFloat(document.getElementById("tip-percentage").value);
+
+        const tipAmount=billAmount*(tipPerecentage/100);
+        const totalAmount=tipAmount+billAmount;
+
+        document.getElementById("tip-amount").textContent=`$`+tipAmount.toFixed(2);
+        document.getElementById("total-amount").textContent=`$`+totalAmount.toFixed(2);
+
+
+
+
+};
